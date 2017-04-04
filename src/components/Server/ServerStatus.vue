e6a1eadee0ef39db62803a850d78fb7af8ebcb68
keisatou <dummy>
2017-04-04 23:49:55 +0900

    factor out listing server status as Component

---
+++ src/components/Server/ServerStatus.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <li
+      class="list-group-item"
+      v-for="server in servers">
+      Server #{{ server.id }} ({{ server.status }})
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    servers: {
+      type: Array,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
+
